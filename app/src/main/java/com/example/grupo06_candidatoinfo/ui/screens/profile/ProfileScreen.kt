f9fb654fa7ed9c922daa7db43c6c10af466c2e4c
Italo Mendoza <105523522+syderkkk@users.noreply.github.com>
2025-10-18 20:53:15 -0500

    feat: crear pantalla de perfil de candidato

--- app/src/main/java/com/example/grupo06_candidatoinfo/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/grupo06_candidatoinfo/ui/screens/profile/ProfileScreen.kt
@@ -1,2 +1,27 @@
 package com.example.grupo06_candidatoinfo.ui.screens.profile
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileScreen(
+    navController: NavController,
+    candidateId: String?
+) {
+}
