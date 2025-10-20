5031080f7a5b0cebc9cab56ba2d905e8c7919c7d
jhamile4 <jhamile.macavilca@tecsup.edu.pe>
2025-10-20 15:59:02 -0500

    commit agregar dependencia

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,47 +42,19 @@ android {
 }
 
 dependencies {
-    // AndroidX Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-
-    // Compose BOM
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-
-    // Navigation Compose
-    implementation("androidx.navigation:navigation-compose:2.9.5")
-
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:3.0.0")
-    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
-
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.3")
-
-
-    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-
-    // Debug
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-
-    // Coil – carga y visualización de imágenes
-    implementation("io.coil-kt:coil-compose:2.7.0")
-
-    //
-    implementation("androidx.compose.material:material-icons-extended-android:1.6.8")
-
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-
-
 }
