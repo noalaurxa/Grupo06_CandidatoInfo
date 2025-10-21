94f06bc7a25a6542ed9dfa633eff862d0aa239c1
noalaurxa <naudy.noa@tecsup.edu.pe>
2025-10-21 15:44:13 -0500

    feat(compare): Añadir estado vacío a CompareScreen

--- app/src/main/java/com/example/grupo06_candidatoinfo/ui/screens/compare/CompareScreen.kt
+++ app/src/main/java/com/example/grupo06_candidatoinfo/ui/screens/compare/CompareScreen.kt
@@ -1,2 +1,11 @@
 package com.example.grupo06_candidatoinfo.ui.screens.compare
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun CompareScreen(
+    navController: NavController,
+) {
+}
