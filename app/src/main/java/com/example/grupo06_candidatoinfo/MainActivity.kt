805d72c5994a33e7a928c5fdee52fc2b58602116
noalaurxa <naudy.noa@tecsup.edu.pe>
2025-10-23 23:39:29 -0500

    refactor: Renombrar componente principal de navegación a NavGraph

--- app/src/main/java/com/example/grupo06_candidatoinfo/MainActivity.kt
+++ app/src/main/java/com/example/grupo06_candidatoinfo/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.example.grupo06_candidatoinfo
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.example.grupo06_candidatoinfo.navigation.SetupNavGraph
+import com.example.grupo06_candidatoinfo.navigation.NavGraph
 import com.example.grupo06_candidatoinfo.ui.theme.Grupo06_CandidatoInfoTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val navController = rememberNavController()
-                    SetupNavGraph(navController = navController)
+                    NavGraph(navController = navController)
                 }
             }
         }
